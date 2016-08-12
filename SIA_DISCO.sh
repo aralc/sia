e536a621624ee8ef91973ac5922bd16257697de2
aralc <mthiago.info@gmail.com>
2016-08-12 00:30:55 -0300

    ATL

--- SIA_DISCO.sh
+++ SIA_DISCO.sh
@@ -6,14 +6,36 @@
 
 SIA_DIS(){
 optD=$(dialog --stdout --title "SIA - ADMIN DISCO" --menu "Administracao Disco" 0 0 0 \
-1 "Verificar espaco disco" \
-2 "Voltar ")
+1 "Analise disco a disco" \
+2 "Visao geral disco" \
+3 "Voltar ")
 	case $optD in 
 		1)
-			disco=$(df -h | cut -d "%" -f1 | tr [a-z] [A-Z])
-		dialog --stdout --title "SIA - ADMIN DISCO" --msgbox "$disco" 0 0 
+		disco=$(df -h | awk '{print $1}')
+		discoT=$(df -h | awk '{print $5}')
+		for dis in $disco
+		do
+		cont=$(expr $cont + 1)		
+		echo ${d[$cont]}
+		d[$cont]=$dis
+		done				
+		
+		for dist in $discoT
+		do
+		cont2=$(expr $cont2 + 1)
+		disco_tam[${cont2}]=$dist
+		done
+		
+		tam=$(df -h | wc -l)
+		echo $tam
+		i=0
+		while [ $i -lt $tam ]
+		do
+		i=$(expr $i + 1)			
+		dialog --stdout --title "SIA - ADMIN DISCO" --msgbox "O disco :: ${d[$i]} Percentual de uso:: ${disco_tam[$i]}" 0 0 
+		done 
 		;;			
-		2) sh $(pwd)/sia.sh
+		3) sh $(pwd)/sia.sh
 		
 		#read -p "Pressione um teclara para continuar" teclado	
 	esac
